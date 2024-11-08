b1bdc1edcbb8df4f8bcd5b39fad190991fbee663
Cael Formanek <cformanek@chapman.edu>
2024-11-07 19:17:22 -0800

    switched to beautiful soup for parsing

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 This app is a passion project built by Cael Formanek to bring Colorado sports into one place. Using web scraping and AI interpretation, the app is able to grab information from top Colorado sports news outlets and present them to the user.
 
 ### To get started
-1st time: run "source setup.sh" <br>
+1st time: source setup.sh <br>
 nth time: source venv/bin/activate <br>
 to deactivate venv: deactivate <br>
--- news_scraper.py
+++ news_scraper.py
@@ -1,11 +1,22 @@
 import requests
+from bs4 import BeautifulSoup
 
-# Making a GET request
-r = requests.get('https://www.geeksforgeeks.org/python-programming-language/')
+# Making a GET request to the page
+r = requests.get('https://www.milehighreport.com/2024/11/7/24290075/broncos-vs-chiefs-3-keys-to-game')
 
-# check status code for response received
-# success code - 200
-print(r)
+# Parsing the HTML, this is the raw content
+soup = BeautifulSoup(r.content, 'html.parser')
 
-# print content of request
-print(r.content)
+# Find the main content div with class 'c-entry-content'
+main_content = soup.find('div', class_='c-entry-content')
+
+# If the div is found, extract and print the text from paragraphs and headings
+if main_content:
+    # Find all paragraphs and headings inside the div
+    content = main_content.find_all(['p', 'h2', 'h3', 'h4'])  # Add more tags if needed
+    
+    # Loop through and print the text of each element
+    for element in content:
+        print(element.text.strip())  # Strip any extra whitespace
+else:
+    print("Content not found")
